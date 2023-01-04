a27046851f6487a204b5e78c73d609c907547b85
Nilima <nilimamahakur18@gmail.com>
2023-01-04 10:15:22 +0530

    another commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 // import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
@@ -8,7 +9,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode> 
+    <HashRouter>
     <App/>
+    </HashRouter>
      </React.StrictMode>
    
    
